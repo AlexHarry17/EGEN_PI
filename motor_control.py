5cdf4030fc90616d6410812c7c295401d3cfdaf9
Alex Harry <alex.harry1@gmail.com>
2019-10-27 10:51:06 -0600

    Added motor control

--- motor_control.py
+++ motor_control.py
@@ -1,26 +1,38 @@
+import gpiozero as gpio
 class MotorControl:
     def __init__(self):
+        # Controling with gpiozero source: https://gpiozero.readthedocs.io/en/stable/api_output.html
         self.move = {'forward': self.move_forward, 'backward': self.move_backward, 'right': self.turn_right, 'left': self.turn_left, 'stop turn': self.stop_turn, 'stop forward/backward': self.stop_forward_reverse}
-
+        self.drive_motor = gpio.Motor(23,24)
+        self.turn_motor = gpio.AngularServo(17,45,-45)
     def function(self, instruction):
         # Running methods from a dictionary souce: https://stackoverflow.com/questions/36849108/calling-a-function-from-within-a-dictionary, user: alecxe
         self.move[instruction]()
 
     def move_forward(self):
+        self.drive_motor.forward(1)
         print("moving forward")
 
     def move_backward(self):
+        self.drive_motor.backward(1)
         print("moving backward")
 
     def turn_right(self):
+        if self.turn_motor.angle < 45:
+            self.turn_motor.angle += 1
         print("turn right")
 
 
     def turn_left(self):
+        if self.turn_motor.angle > -45:
+            self.turn_motor.angle -= 1
+
         print("turn left")
 
     def stop_turn(self):
         print('stop turn')
 
     def stop_forward_reverse(self):
-        print(' stop forward back')
+        self.drive_motor.forward(0)
+        self.drive_motor.backward(0)
+        print('stop forward back')
