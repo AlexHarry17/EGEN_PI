d959b6fcd0a1e65d3d26174a4166b09c7cd9e072
Alex Harry <alex.harry1@gmail.com>
2019-11-03 18:24:13 -0700

    Reconnects

--- network.py
+++ network.py
@@ -3,26 +3,27 @@ import base64
 from motor_control import MotorControl
 class Network:
    def __init__(self):
-      UDP_IP = "192.168.1.145"
-      UDP_PORT = 0
+      bluetooth_address = 'B8:27:EB:9B:0D:DF'
+      bluetooth_port = 3
    # Code for connecting over wifi source: https://wiki.python.org/moin/UdpCommunication
-      self.app_socket = socket.socket(socket.AF_INET, # Internet
-                           socket.SOCK_DGRAM) # UDP
-      self.app_socket.bind((UDP_IP, UDP_PORT))
-
-      port = int(str(self.app_socket).split(',')[5].replace(')>', ''))
-      print("Port: ", port)
-      self.send_network_info(UDP_IP,port)
+      self.app_socket = socket.socket(socket.AF_BLUETOOTH, # Internet
+                           socket.SOCK_STREAM, socket.BTPROTO_RFCOMM ) # Bluetooth socket connection
+      self.app_socket.bind((bluetooth_address, bluetooth_port))
+      self.app_socket.listen(1)
       self.get_data(MotorControl())
-      app_socket.close()
+      self.app_socket.close()
 
    def get_data(self, motor_control):
+      client, address = self.app_socket.accept()
       while True:
-         data, addr = self.app_socket.recvfrom(4096) # buffer size is 1024 bytes
+         data = client.recv(4096) # buffer size is 1024 bytes
          # Source to decode data from https://stackoverflow.com/questions/35673809/convert-file-to-base64-string-on-python-3, user: tdelaney
-         data = data.decode('ascii')
-         motor_control.function(data)
-
+         if data:
+            data = data.decode('ascii')
+            try:
+               motor_control.function(data)
+            except:
+               print("Incorrect Movement")
    def send_network_info(self,ip,port):
       while True:
          print('sending on port ', port)
