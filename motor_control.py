ee0577901a27b4fa743db4f22feaa058907e0f0c
Alex Harry <alex.harry1@gmail.com>
2019-11-18 21:03:22 -0700

    showing ip and port

--- motor_control.py
+++ motor_control.py
@@ -2,10 +2,15 @@ import gpiozero as gpio
 import time
 import RPi.GPIO as GPIO          
 from time import sleep
+# import Raspi_MotorHAT
 
 in1 = 24
 in2 = 23
 en = 25
+in3 = 16
+in4 = 20
+en2 = 21
+
 temp1=1
 
 GPIO.setmode(GPIO.BCM)
@@ -14,9 +19,16 @@ GPIO.setup(in2,GPIO.OUT)
 GPIO.setup(en,GPIO.OUT)
 GPIO.output(in1,GPIO.LOW)
 GPIO.output(in2,GPIO.LOW)
+GPIO.setup(in3,GPIO.OUT)
+GPIO.setup(in4,GPIO.OUT)
+GPIO.setup(en2,GPIO.OUT)
+GPIO.output(in3,GPIO.LOW)
+GPIO.output(in4,GPIO.LOW)
 p=GPIO.PWM(en,50)
+p2=GPIO.PWM(en2,50)
 
 p.start(25)
+p2.start(en2)
 print("\n")
 print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
@@ -29,18 +41,25 @@ class MotorControl:
         # Controling with gpiozero source: https://gpiozero.readthedocs.io/en/stable/api_output.html
         self.move = {'forward': self.move_forward, 'backward': self.move_backward, 'right': self.turn_right, 'left': self.turn_left, 'stop turn': self.stop_turn, 'stop forward/backward': self.stop_forward_reverse}
         self.drive_motor = gpio.Motor(23,24)
-        self.turn_motor = gpio.AngularServo(17,45,-45)
+        self.drive_motor_2 = gpio.Motor(20,16)
 
+        self.turn_motor = gpio.AngularServo(0,45,-45)
+        # self.mh =Raspi_MotorHAT(addr=0x6F)
+        # self.motor_1 = self.mh.getMotor(1)
     def function(self, instruction):
         # Running methods from a dictionary souce: https://stackoverflow.com/questions/36849108/calling-a-function-from-within-a-dictionary, user: alecxe
        self.move[instruction]()
 
     def move_forward(self):
         self.drive_motor.forward(1) # 1 moves forward, 0 stops motor
+        self.drive_motor_2.forward(1) # 1 moves forward, 0 stops motor
+
         print("moving forward")
 
     def move_backward(self):
         self.drive_motor.backward(1) # 1 Moves backward, 0 stops motor
+        self.drive_motor_2.backward(1) # 1 moves forward, 0 stops motor
+
         print("moving backward")
 
     def turn_right(self):# Turns vehicle right
@@ -61,4 +80,6 @@ class MotorControl:
     def stop_forward_reverse(self): # Stops forward / backward movement
         self.drive_motor.forward(0)
         self.drive_motor.backward(0)
+        self.drive_motor_2.forward(0)
+        self.drive_motor_2.backward(0)
         print('stop forward back')
--- network.py
+++ network.py
@@ -1,16 +1,20 @@
 import socket
 import base64
 from motor_control import MotorControl
+import stats
+import threading
 class Network:
    def __init__(self):
-      UDP_IP = "192.168.1.145"
+      UDP_IP = socket.gethostbyname(socket.gethostname()) # Get ip source: https://stackoverflow.com/questions/48606440/get-ip-address-from-python, user: An0n
       UDP_PORT = 0
    # Code for connecting over wifi source: https://wiki.python.org/moin/UdpCommunication
       self.app_socket = socket.socket(socket.AF_INET, # Internet
                            socket.SOCK_DGRAM) # UDP
       self.app_socket.bind((UDP_IP, UDP_PORT))
-
       port = int(str(self.app_socket).split(',')[5].replace(')>', ''))
+
+      stats.display(UDP_IP, str(port))
+
       print("Port: ", port)
       # self.send_network_info(UDP_IP,port)
       self.get_data(MotorControl())
